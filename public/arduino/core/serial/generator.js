c7436d39714ffb2b826b2453cabfdbb61714efa0
downeyin <853384781@qq.com>
2025-01-08 22:51:29 +0800

    ci: math\logic\serial代码生成器

--- public/arduino/core/serial/generator.js
+++ public/arduino/core/serial/generator.js
@@ -1,31 +1,37 @@
-Arduino.forBlock['serial_begin'] = function (block) {
-    const baud = block.getFieldValue('BAUD');
-    return `Serial.begin(${baud});\n`;
-}
+Arduino.forBlock["serial_begin"] = function (block) {
+  const obj = block.getFieldValue("SERIAL");
+  const speed = block.getFieldValue("SPEED");
+  return `${obj}.begin(${speed});\n`;
+};
 
-Arduino.forBlock['serial_print'] = function (block) {
-    const content = Arduino.valueToCode(block, 'CONTENT', Arduino.ORDER_ATOMIC);
-    return `Serial.print(${content});\n`;
-}
+Arduino.forBlock["serial_print"] = function (block) {
+  const obj = block.getFieldValue("SERIAL");
+  const content = Arduino.valueToCode(block, "VAR", Arduino.ORDER_ATOMIC);
+  return `${obj}.print(${content});\n`;
+};
 
-Arduino.forBlock['serial_println'] = function (block) {
-    const content = Arduino.valueToCode(block, 'CONTENT', Arduino.ORDER_ATOMIC);
-    return `Serial.println(${content});\n`;
-}
+Arduino.forBlock["serial_println"] = function (block) {
+  const obj = block.getFieldValue("SERIAL");
+  const content = Arduino.valueToCode(block, "VAR", Arduino.ORDER_ATOMIC);
+  return `${obj}.println(${content});\n`;
+};
 
-Arduino.forBlock['serial_read'] = function (block) {
-    return 'Serial.read()';
-}
+Arduino.forBlock["serial_read"] = function (block) {
+  const obj = block.getFieldValue("SERIAL");
+  return [`${obj}.read()`, Arduino.ORDER_FUNCTION_CALL];
+};
 
-Arduino.forBlock['serial_available'] = function (block) {
-    return 'Serial.available()';
-}
+Arduino.forBlock["serial_available"] = function (block) {
+  const obj = block.getFieldValue("SERIAL");
+  return [`${obj}.available()`, Arduino.ORDER_FUNCTION_CALL];
+};
 
-Arduino.forBlock['serial_flush'] = function (block) {
-    return 'Serial.flush();\n';
-}
+Arduino.forBlock["serial_flush"] = function (block) {
+  const obj = block.getFieldValue("SERIAL");
+  return `${obj}.flush();\n`;
+};
 
-Arduino.forBlock['serial_parseint'] = function (block) {
-    return 'Serial.parseInt()';
-}
-
+Arduino.forBlock["serial_parseint"] = function (block) {
+  const obj = block.getFieldValue("SERIAL");
+  return [`${obj}.parseInt()`, Arduino.ORDER_FUNCTION_CALL];
+};
