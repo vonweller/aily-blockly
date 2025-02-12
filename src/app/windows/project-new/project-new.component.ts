5f8404812ffe013d778d820acb00b1f693817b86
coloz <coloz999@gmail.com>
2025-02-12 12:08:27 +0800

    添加开发板选择相关

--- src/app/windows/project-new/project-new.component.ts
+++ src/app/windows/project-new/project-new.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzStepsModule } from 'ng-zorro-antd/steps';
+import { ElectronService } from '../../services/electron.service';
 
 @Component({
   selector: 'app-project-new',
@@ -23,6 +24,15 @@ export class ProjectNewComponent {
 
   currentStep = 0;
 
+  boards = [
+    { value: 'arduino', name: 'Arduino', img: 'assets/arduino.png' },
+    { value: 'esp8266', name: 'ESP8266', img: 'assets/arduino.png' },
+    { value: 'esp32', name: 'ESP32', img: 'assets/arduino.png' },
+    { value: 'raspberry', name: 'Raspberry', img: 'assets/arduino.png' },
+    { value: 'stm32', name: 'STM32', img: 'assets/arduino.png' },
+    { value: 'other', name: 'Other', img: 'assets/arduino.png' },
+  ]
+
   projectData = {
     name: 'new project',
     path: '',
@@ -33,8 +43,20 @@ export class ProjectNewComponent {
     version: '1.0.0',
   }
 
+  constructor(
+    private electronService: ElectronService
+  ) {
+
+  }
+
   ngOnInit() {
-    this.projectData.path = window['path'].getUserDocuments();
+    if (this.electronService.isElectron) {
+      this.projectData.path = window['path'].getUserDocuments();
+    }
+  }
+
+  selectBoard(board){
+    
   }
 
   async selectFolder() {
