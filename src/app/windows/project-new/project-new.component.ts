adfbf2eb74fe1c877cd764a1bad785f88cbdfef7
coloz <coloz999@gmail.com>
2025-02-13 11:20:15 +0800

    完善项目创建

--- src/app/windows/project-new/project-new.component.ts
+++ src/app/windows/project-new/project-new.component.ts
@@ -15,39 +15,58 @@ import { ElectronService } from '../../services/electron.service';
     SubWindowComponent,
     NzButtonModule,
     NzInputModule,
-    NzStepsModule
+    NzStepsModule,
   ],
   templateUrl: './project-new.component.html',
-  styleUrl: './project-new.component.scss'
+  styleUrl: './project-new.component.scss',
 })
 export class ProjectNewComponent {
-
   currentStep = 0;
 
   boards = [
-    { value: 'arduino', name: 'Arduino', img: 'assets/arduino.png' },
-    { value: 'esp8266', name: 'ESP8266', img: 'assets/arduino.png' },
-    { value: 'esp32', name: 'ESP32', img: 'assets/arduino.png' },
-    { value: 'raspberry', name: 'Raspberry', img: 'assets/arduino.png' },
-    { value: 'stm32', name: 'STM32', img: 'assets/arduino.png' },
-    { value: 'other', name: 'Other', img: 'assets/arduino.png' },
-  ]
+    {
+      value: '@blockly/board/arduino_uno@1.0.0',
+      name: 'Arduino UNO R3',
+      img: 'board/arduino_uno/arduino_uno.png',
+      desc: 'Arduino UNO R3 是一款基于ATmega328P的开源电子原型平台，包含数字I/O口14个（其中6个支持PWM输出），模拟输入口6个，16MHz晶振，USB接口，电源接口，ICSP接口等。',
+    },
+    {
+      value: '@blockly/board/arduino_mega@1.0.0',
+      name: 'Arduino MEGA',
+      img: 'board/arduino_mega/arduino_mega.png',
+      desc: 'Arduino MEGA 是一款基于ATmega2560的开源电子原型平台，包含数字I/O口54个（其中14个支持PWM输出），模拟输入口16个，16MHz晶振，USB接口，电源接口，ICSP接口等。',
+    },
+    {
+      value: '@blockly/board/esp32c3@1.0.0',
+      name: 'esp32c3',
+      img: 'board/esp32c3/esp32c3.png',
+      desc: 'esp32c3 是一款基于ESP32-C3的开源电子原型平台，包含数字I/O口14个（其中6个支持PWM输出），模拟输入口6个，2.4GHz Wi-Fi，蓝牙5.0，USB接口，电源接口，ICSP接口等。',
+    },
+    {
+      value: '@blockly/board/raspberrypi_pico@1.0.0',
+      name: 'RaspberryPi Pico',
+      img: 'board/raspberrypi_pico/raspberrypi_pico.png',
+      desc: 'RaspberryPi Pico 是一款基于RP2040的开源电子原型平台，包含数字I/O口26个（其中16个支持PWM输出），模拟输入口3个，133MHz主频，USB接口，电源接口，ICSP接口等。',
+    },
+    {
+      value: '@blockly/board/wifiduino32@1.0.0',
+      name: 'Wifiduino32',
+      img: 'board/wifiduino32/wifiduino32.png',
+      desc: 'Wifiduino32 是一款基于ESP32的开源电子原型平台，包含数字I/O口38个（其中6个支持PWM输出），模拟输入口6个，2.4GHz Wi-Fi，蓝牙4.2，USB接口，电源接口，ICSP接口等。',
+    },
+  ];
 
   projectData = {
     name: 'new project',
     path: '',
     description: '',
-    board: 'arduino',
+    board: this.boards[0].value,
     type: 'web',
     framework: 'angular',
     version: '1.0.0',
-  }
+  };
 
-  constructor(
-    private electronService: ElectronService
-  ) {
-
-  }
+  constructor(private electronService: ElectronService) {}
 
   ngOnInit() {
     if (this.electronService.isElectron) {
@@ -55,18 +74,20 @@ export class ProjectNewComponent {
     }
   }
 
-  selectBoard(board){
-    
+  currentBoard = this.boards[0];
+  selectBoard(board) {
+    this.currentBoard = board;
+    this.projectData.board = board.value;
   }
 
   async selectFolder() {
-    const folderPath = await window['ipcRenderer'].invoke("select-folder", { path: this.projectData.path });
-    console.log("选中的文件夹路径：", folderPath);
+    const folderPath = await window['ipcRenderer'].invoke('select-folder', {
+      path: this.projectData.path,
+    });
+    console.log('选中的文件夹路径：', folderPath);
     this.projectData.path = folderPath;
     // 在这里对返回的 folderPath 进行后续处理
   }
 
-  createProject() {
-
-  }
+  createProject() {}
 }
