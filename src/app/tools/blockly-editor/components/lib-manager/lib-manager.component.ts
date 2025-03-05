8a54eee2b7dd672253fe538be40db7c5e0417c3d
coloz <coloz999@gmail.com>
2025-03-05 16:08:33 +0800

    完善页面

--- src/app/tools/blockly-editor/components/lib-manager/lib-manager.component.ts
+++ src/app/tools/blockly-editor/components/lib-manager/lib-manager.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { NpmService } from '../../../../services/npm.service';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lib-manager',
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule,
+    NzInputModule,
+    NzButtonModule,
+    NzToolTipModule,
+    NzSelectModule 
+  ],
   templateUrl: './lib-manager.component.html',
   styleUrl: './lib-manager.component.scss'
 })
 export class LibManagerComponent {
 
+  @Output() close = new EventEmitter();
+
   libList: PackageInfo[] = [];
+
+  version;
+  versionList=[]
 
   constructor(
     private npmService: NpmService
@@ -22,19 +40,40 @@ export class LibManagerComponent {
     })
   }
 
+  processForSearch(array) {
+    for (let index = 0; index < array.length; index++) {
+      const item = array[index];
+      item['fulltext'] = `${item.nickname} ${item.description} ${item.keywords.join(' ')}${item.brand} ${item.author.name}`;
+    }
+  }
+
+  back() {
+    this.close.emit();
+  }
+
+  installLib(lib){
+
+  }
+
+  removeLib(lib){
+
+  }
 }
 
 interface PackageInfo {
   "name": string,
-  "scope": string,
-  "description": string,
-  "version": string,
-  "keywords": string[],
-  "date": string,
-  "author": {
-    "name": string
+  "nickname": string,
+  "scope"?: string,
+  "description"?: string,
+  "version"?: string,
+  "keywords"?: string[],
+  "date"?: string,
+  "author"?: {
+    "name"?: string
   },
-  "publisher": any,
-  "maintainers": any[],
-  "links": any
+  "publisher"?: any,
+  "maintainers"?: any[],
+  "links"?: any,
+  "brand"?:string,
+  "fulltext"?: string
 }
