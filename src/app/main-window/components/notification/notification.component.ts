700b4351ae789371ce22d658c80fbcea6174f955
coloz <coloz999@gmail.com>
2025-03-07 11:11:09 +0800

    添加弹出通知组件

---
+++ src/app/main-window/components/notification/notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+
+@Component({
+  selector: 'app-notification',
+  imports: [NzProgressModule],
+  templateUrl: './notification.component.html',
+  styleUrl: './notification.component.scss'
+})
+export class NotificationComponent {
+
+}
