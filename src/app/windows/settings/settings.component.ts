e322e2dd7ae08c36d97f833aea129c252ba20954
coloz <coloz999@gmail.com>
2025-04-10 15:35:47 +0800

    优化设置页面布局

--- src/app/windows/settings/settings.component.ts
+++ src/app/windows/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { SubWindowComponent } from '../../components/sub-window/sub-window.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzInputModule } from 'ng-zorro-antd/input';
@@ -8,7 +8,8 @@ import { UiService } from '../../services/ui.service';
 import { NzRadioModule } from 'ng-zorro-antd/radio';
 import { SettingsService } from '../../services/settings.service';
 import { TranslationService } from '../../services/translation.service';
-
+import { ConfigService } from '../../services/config.service';
+import { SimplebarAngularModule } from 'simplebar-angular';
 @Component({
   selector: 'app-settings',
   imports: [
@@ -17,12 +18,23 @@ import { TranslationService } from '../../services/translation.service';
     SubWindowComponent,
     NzButtonModule,
     NzInputModule,
-    NzRadioModule
+    NzRadioModule,
+    SimplebarAngularModule
   ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
 })
 export class SettingsComponent {
+  @ViewChild('scrollContainer', { static: false }) scrollContainer: ElementRef;
+  
+  activeSection = '基础设置'; // 当前活动的部分
+
+  // simplebar 配置选项
+  options = {
+    autoHide: true,
+    scrollbarMinSize: 50
+  };
+
   items = [
     {
       name: '基础设置',
@@ -33,18 +45,18 @@ export class SettingsComponent {
       ],
     },
     {
-      name: '编译设置',
-      icon: 'fa-light fa-screwdriver-wrench',
-      content: [
-
-      ],
-    },
-    {
       name: '主题设置',
       icon: 'fa-light fa-gift',
       content: [
         { name: '主题', key: 'theme.theme' },
         { name: '字体', key: 'theme.font' },
+      ],
+    },
+    {
+      name: '编译设置',
+      icon: 'fa-light fa-screwdriver-wrench',
+      content: [
+
       ],
     },
     {
@@ -65,8 +77,6 @@ export class SettingsComponent {
   npmRegistryListStr = `https://registry.npmjs.org/
 https://registry.npm.taobao.org/`;
 
-  // boardList=[,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,]
-
   get boardList() {
     return this.settingsService.boardList;
   }
@@ -79,31 +89,67 @@ https://registry.npm.taobao.org/`;
     return this.translationService.getSelectedLanguage();
   }
 
-  data: any = {
-    project_path: '',
-    npm_registry: [],
-  };
+  get data() {
+    return this.configService.data;
+  }
 
   constructor(
     private uiService: UiService,
     private settingsService: SettingsService,
-    private translationService: TranslationService
+    private translationService: TranslationService,
+    private configService: ConfigService
   ) {
-    this.init();
   }
 
-  init() {
-    console.log('init settings');
+  async ngOnInit() {
+    await this.configService.init()
   }
 
   selectLang(lang) {
     this.translationService.setLanguage(lang.code);
-    window['ipcRenderer'].send('setting-changed', {action:'language-changed', data: lang.code});
+    window['ipcRenderer'].send('setting-changed', { action: 'language-changed', data: lang.code });
   }
 
-  currentType = this.items[0];
-  selectType(item) {
-    this.currentType = item;
+  // 使用锚点滚动到指定部分
+  scrollToSection(item) {
+    this.activeSection = item.name;
+    const element = document.getElementById(`section-${item.name}`);
+    if (element && this.scrollContainer) {
+      // 针对simplebar调整滚动方法
+      const simplebarInstance = this.scrollContainer['SimpleBar'];
+      if (simplebarInstance) {
+        simplebarInstance.getScrollElement().scrollTo({
+          top: element.offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+
+  // 监听滚动事件以更新活动菜单项
+  onScroll() {
+    const sections = document.querySelectorAll('.section');
+    let scrollElement;
+    
+    // 获取simplebar的滚动元素
+    const simplebarInstance = this.scrollContainer['SimpleBar'];
+    if (simplebarInstance) {
+      scrollElement = simplebarInstance.getScrollElement();
+    } else {
+      return;
+    }
+    
+    const scrollPosition = scrollElement.scrollTop;
+    
+    sections.forEach((section: HTMLElement) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      
+      if (scrollPosition >= sectionTop - 50 && 
+          scrollPosition < sectionTop + sectionHeight - 50) {
+        this.activeSection = section.id.replace('section-', '');
+      }
+    });
   }
 
   cancel() {
