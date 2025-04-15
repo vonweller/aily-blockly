7555b48e06204653e26df75377744771af164bf4
coloz <coloz999@gmail.com>
2025-04-15 20:55:50 +0800

    更新

---
+++ src/app/tools/simulator/readme.md
@@ -0,0 +1,5 @@
+# todo
+使用项目： 
+https://github.com/wokwi/wokwi-elements  
+https://github.com/wokwi/wokwi-gdbserver  
+https://github.com/wokwi/avr8js  
---
+++ src/app/tools/simulator/simulator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simulator',
+  imports: [],
+  templateUrl: './simulator.component.html',
+  styleUrl: './simulator.component.scss'
+})
+export class SimulatorComponent {
+
+}
