ca902e91c303c45c5ffa624eea9dd01d8f02bfe9
coloz <coloz999@gmail.com>
2025-04-24 01:12:14 +0800

    研究esp32模拟方式；

--- src/app/tools/simulator/readme.md
+++ src/app/tools/simulator/readme.md
@@ -3,3 +3,9 @@
 https://github.com/wokwi/wokwi-elements  
 https://github.com/wokwi/wokwi-gdbserver  
 https://github.com/wokwi/avr8js  
+
+
+## ESP32  
+对于EPS32由于wokwi没有提供esp32版本模拟器。
+可以直接运行[qemu](https://github.com/espressif/qemu)实现模拟，然后通过qemu提供的监视器接口获取硬件状态。
+
--- src/app/tools/simulator/simulator.service.ts
+++ src/app/tools/simulator/simulator.service.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@angular/core';
+import {
+  avrInstruction,
+  AVRTimer,
+  CPU,
+  timer0Config,
+  AVRUSART,
+  usart0Config,
+  AVRIOPort,
+  portBConfig,
+  portCConfig,
+  portDConfig
+} from "avr8js";
+import { AVRRunner } from './avrruner';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SimulatorService {
 
+  runner: AVRRunner;
+
   constructor() { }
 
 
   loadBinary(file) {
+
+  }
+
+  run() {
 
   }
 }
