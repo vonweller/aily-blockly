a42d34f9d7069be0365541c29feeba52370267b8
coloz <coloz999@gmail.com>
2025-05-03 14:48:41 +0800

    完善示例页面

--- src/app/pages/playground/subject-item/subject-item.component.ts
+++ src/app/pages/playground/subject-item/subject-item.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { ITEM_LIST } from '../data';
+import { SimplebarAngularModule } from 'simplebar-angular';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-subject-item',
-  imports: [],
+  imports: [SimplebarAngularModule, NzButtonModule],
   templateUrl: './subject-item.component.html',
   styleUrl: './subject-item.component.scss'
 })
 export class SubjectItemComponent {
 
   itemList = ITEM_LIST
+
+  options = {
+    autoHide: true,
+    clickOnTrack: true,
+    scrollbarMinSize: 50,
+  };
 }
