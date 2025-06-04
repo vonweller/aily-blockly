f578d094d55962691f90ff6933aff86910626227
coloz <coloz999@gmail.com>
2025-06-04 23:10:02 +0800

    更新

--- src/app/tools/aily-chat/aily-chat.component.ts
+++ src/app/tools/aily-chat/aily-chat.component.ts
@@ -53,14 +53,18 @@ export class AilyChatComponent {
       role: 'user',
     }, {
       content: `推荐使用如下控制器：  
-\`\`\`aily-state
-{"id":"state-89765221","text":"正在查询开发板信息..."}
-\`\`\`
 \`\`\`aily-board
 {
-"name": "Arduino Uno",
-"package":"@aily-project/board-arduino-uno",
-"text": "Arduino Unoddfadsfdasfasdkljkljlkj",
+    "name": "@aily-project/board-jinniu_board",
+    "nickname": "金牛创翼板",
+    "version": "0.0.1",
+    "description": "金牛创翼板是一款集成多种常用传感器的开发板，包括电机、WS2812灯、LED灯、超声波、DHT11、自锁和按键开关、电位器、无源蜂鸣器和电机驱动",
+    "author": "",
+    "brand": "OpenJumper",
+    "url": "",
+    "compatibility": "",
+    "img": "jinniu_board.png",
+    "disabled": false
 }
 \`\`\``,
     },
@@ -70,13 +74,84 @@ export class AilyChatComponent {
     }, {
       content: `推荐使用如下扩展库
 \`\`\`aily-library
-{}
+{
+    "name": "@aily-project/lib-servo360",
+    "nickname": "360舵机驱动",
+    "version": "1.0.0",
+    "description": "360舵机控制支持库，支持Arduino UNO、MEGA、ESP32等开发板",
+    "author": "aily Project",
+    "compatibility": {
+      "core": [
+        "arduino:avr",
+        "esp32:esp32"
+      ],
+      "voltage": [
+        3.3,
+        5
+      ]
+    },
+    "keywords": [
+      "aily",
+      "blockly",
+      "servo",
+      "servo_attach",
+      "servo_write",
+      "执行器"
+    ],
+    "tested": true,
+    "icon": "iconfont icon-servo"
+}
 \`\`\`
 \`\`\`aily-library
-{}
+{
+    "name": "@aily-project/lib-sht3x",
+    "nickname": "SHT3x温湿度传感器库",
+    "version": "0.0.1",
+    "description": "支持Arduino SHT30、SHT31和SHT35温湿度传感器的控制库",
+    "author": "Danil",
+    "compatibility": {
+      "core": [
+        "arduino:avr",
+        "esp32:esp32"
+      ],
+      "voltage": [
+        3.3,
+        5
+      ]
+    },
+    "keywords": [
+      "aily",
+      "blockly",
+      "sht3x",
+      "温湿度传感器",
+      "sensor",
+      "humidity",
+      "temperature"
+    ],
+    "tested": false,
+    "icon": "iconfont icon-dht22"
+}
 \`\`\`
 \`\`\`aily-library
-{}
+{
+    "name": "@aily-project/lib-core-custom",
+    "nickname": "自定义代码",
+    "version": "1.0.0",
+    "description": "允许在Blockly中插入自定义Arduino代码、宏定义、函数等的库",
+    "author": "aily Project",
+    "compatibility": {
+      "core": []
+    },
+    "keywords": [
+      "aily",
+      "blockly",
+      "lib",
+      "custom",
+      "code"
+    ],
+    "tested": true,
+    "icon": "fa-light fa-code"
+}
 \`\`\`
 `
     },
