ff409ecda47197cd56d056ff32df81a9fa69ad12
coloz <coloz999@gmail.com>
2025-07-31 15:42:35 +0800

    允许查看pinmap和开发板文档

--- src/app/components/float-sider/float-sider.component.ts
+++ src/app/components/float-sider/float-sider.component.ts
@@ -1,8 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { PinmapComponent } from '../../app-store/pinmap/pinmap.component';
 import { NzModalModule, NzModalService } from 'ng-zorro-antd/modal';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { CommonModule } from '@angular/common';
+import { ProjectService } from '../../services/project.service';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { ElectronService } from '../../services/electron.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 @Component({
   selector: 'app-float-sider',
   imports: [
@@ -13,14 +19,58 @@ import { CommonModule } from '@angular/common';
   templateUrl: './float-sider.component.html',
   styleUrl: './float-sider.component.scss'
 })
-export class FloatSiderComponent {
+export class FloatSiderComponent implements OnInit, OnDestroy {
   @Input() show = false;
 
+  loaded = false;
+  private routerSubscription: Subscription | undefined;
+
   constructor(
-    private modal: NzModalService
+    private modal: NzModalService,
+    private projectService: ProjectService,
+    private router: Router,
+    private electronService: ElectronService,
+    private message: NzMessageService
   ) { }
 
+  ngOnInit() {
+    // 监听路由变化
+    if (this.router.url.indexOf('/main/blockly-editor') !== -1) {
+      this.loaded = true;
+      this.loadBoardInfo();
+    }
+    this.routerSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        if (event.url.indexOf('/main/blockly-editor') !== -1) {
+          this.loaded = true;
+          this.loadBoardInfo();
+        } else {
+          this.loaded = false;
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    // 清理订阅
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
+
+  boardPackagePath;
+  async loadBoardInfo() {
+    setTimeout(async () => {
+      this.boardPackagePath = await this.projectService.getBoardPackagePath();
+      console.log('Board Package Path:', this.boardPackagePath);
+    }, 1000); // 延时1秒，确保项目服务已准备好
+  }
+
   showPinmap() {
+    if (!this.electronService.exists(this.boardPackagePath + '/pinmap.webp')) {
+      this.message.error('该开发板没有提供引脚图');
+      return;
+    }
     const modalRef = this.modal.create({
       nzTitle: null,
       nzFooter: null,
@@ -30,9 +80,19 @@ export class FloatSiderComponent {
       },
       nzContent: PinmapComponent,
       nzData: {
-        boardPackage: 'arduino_uno'
+        img: this.boardPackagePath + '/pinmap.webp' // 假设 pinmap 图片路径
       },
-      nzWidth: '650px',
+      nzWidth: '500px',
     });
+  }
+
+
+  openDocUrl() {
+    let data = JSON.parse(this.electronService.readFile(this.boardPackagePath + '/package.json'))
+    if (!data.url) {
+      this.message.error('该开发板没有提供文档链接');
+      return;
+    }
+    this.electronService.openUrl(data.url)
   }
 }
