df321a694abdb5235292e05994fa3f3b6f45ad6e
coloz <coloz999@gmail.com>
2025-08-01 15:55:19 +0800

    修复文件加载

--- src/app/editors/code-editor/components/file-tree/file-tree.component.ts
+++ src/app/editors/code-editor/components/file-tree/file-tree.component.ts
@@ -1,12 +1,13 @@
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { CollectionViewer, DataSource, SelectionChange } from '@angular/cdk/collections';
 import { FlatTreeControl } from '@angular/cdk/tree';
 import { SelectionModel } from '@angular/cdk/collections';
-import { NzTreeFlatDataSource, NzTreeFlattener, NzTreeViewModule } from 'ng-zorro-antd/tree-view';
+import { NzTreeViewModule } from 'ng-zorro-antd/tree-view';
 import { FileService } from '../../file.service';
 import { SimplebarAngularModule } from 'simplebar-angular';
 import { CommonModule } from '@angular/common';
-import { BehaviorSubject } from 'rxjs';
-import { NzTreeNodeOptions } from 'ng-zorro-antd/tree';
+import { BehaviorSubject, Observable, merge } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 // 原始文件节点接口
 interface FileNode {
@@ -25,6 +26,101 @@ interface FlatFileNode {
   key: string;
   isLeaf: boolean;
   path: string;
+  loading?: boolean;
+}
+
+// 动态数据源类
+class DynamicFileDataSource implements DataSource<FlatFileNode> {
+  private flattenedData: BehaviorSubject<FlatFileNode[]>;
+  private childrenLoadedSet = new Set<FlatFileNode>();
+
+  constructor(
+    private treeControl: FlatTreeControl<FlatFileNode>,
+    private fileService: FileService,
+    initData: FlatFileNode[]
+  ) {
+    this.flattenedData = new BehaviorSubject<FlatFileNode[]>(initData);
+    treeControl.dataNodes = initData;
+  }
+
+  connect(collectionViewer: CollectionViewer): Observable<FlatFileNode[]> {
+    const changes = [
+      collectionViewer.viewChange,
+      this.treeControl.expansionModel.changed.pipe(tap(change => this.handleExpansionChange(change))),
+      this.flattenedData.asObservable()
+    ];
+    return merge(...changes).pipe(map(() => this.expandFlattenedNodes(this.flattenedData.getValue())));
+  }
+
+  expandFlattenedNodes(nodes: FlatFileNode[]): FlatFileNode[] {
+    const treeControl = this.treeControl;
+    const results: FlatFileNode[] = [];
+    const currentExpand: boolean[] = [];
+    currentExpand[0] = true;
+
+    nodes.forEach(node => {
+      let expand = true;
+      for (let i = 0; i <= treeControl.getLevel(node); i++) {
+        expand = expand && currentExpand[i];
+      }
+      if (expand) {
+        results.push(node);
+      }
+      if (treeControl.isExpandable(node)) {
+        currentExpand[treeControl.getLevel(node) + 1] = treeControl.isExpanded(node);
+      }
+    });
+    return results;
+  }
+
+  handleExpansionChange(change: SelectionChange<FlatFileNode>): void {
+    if (change.added) {
+      change.added.forEach(node => this.loadChildren(node));
+    }
+  }
+
+  loadChildren(node: FlatFileNode): void {
+    if (this.childrenLoadedSet.has(node)) {
+      return;
+    }
+    node.loading = true;
+    
+    // 使用 fileService 加载子文件夹内容
+    const children = this.fileService.readDir(node.path);
+    const flatChildren: FlatFileNode[] = children.map(child => ({
+      expandable: !child.isLeaf,
+      title: child.title,
+      level: node.level + 1,
+      key: child.key,
+      isLeaf: child.isLeaf,
+      path: child['path']
+    }));
+
+    node.loading = false;
+    const flattenedData = this.flattenedData.getValue();
+    const index = flattenedData.indexOf(node);
+    if (index !== -1) {
+      flattenedData.splice(index + 1, 0, ...flatChildren);
+      this.childrenLoadedSet.add(node);
+    }
+    this.flattenedData.next(flattenedData);
+  }
+
+  disconnect(): void {
+    this.flattenedData.complete();
+  }
+
+  // 更新根数据
+  setRootData(data: FlatFileNode[]): void {
+    this.childrenLoadedSet.clear();
+    this.flattenedData.next(data);
+    this.treeControl.dataNodes = data;
+  }
+
+  // 获取当前数据
+  getCurrentData(): FlatFileNode[] {
+    return this.flattenedData.getValue();
+  }
 }
 
 @Component({
@@ -40,8 +136,6 @@ export class FileTreeComponent implements OnInit {
   @Output() selectedFileChange = new EventEmitter();
 
   isLoading = false;
-  // 维护一个数据副本
-  private dataChange = new BehaviorSubject<FileNode[]>([]);
 
   options = {
     autoHide: true,
@@ -49,23 +143,10 @@ export class FileTreeComponent implements OnInit {
     scrollbarMinSize: 50,
   };
 
-  // 转换器 - 将原始节点转换为扁平节点
-  private transformer = (node: FileNode, level: number): FlatFileNode => ({
-    expandable: !node.isLeaf,
-    title: node.title,
-    level,
-    key: node.key,
-    isLeaf: node.isLeaf,
-    path: node.path
-  });
-
   // 选择模型 - 用于跟踪选中的节点
   nodeSelection = new SelectionModel<FlatFileNode>();
 
   // 树控件 - 使用 FlatTreeControl
-  // 注意：虽然 FlatTreeControl 在 Angular 19 中被标记为已弃用，
-  // 但 ng-zorro-antd 仍然需要它。我们需要等待 ng-zorro-antd 更新到新的 API。
-  // 这个弃用警告是暂时的，不会影响应用程序的运行。
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore - 已知的弃用警告，等待 ng-zorro-antd 更新
   treeControl = new FlatTreeControl<FlatFileNode>(
@@ -73,21 +154,14 @@ export class FileTreeComponent implements OnInit {
     node => node.expandable
   );
 
-  // 树扁平化器
-  treeFlattener = new NzTreeFlattener(
-    this.transformer,
-    node => node.level,
-    node => node.expandable,
-    node => node.children
-  );
-
-  // 数据源
-  dataSource = new NzTreeFlatDataSource(this.treeControl, this.treeFlattener);
+  // 动态数据源
+  dataSource: DynamicFileDataSource;
 
   constructor(
     private fileService: FileService
   ) {
-    this.dataChange.subscribe(data => this.dataSource.setData(data));
+    // 初始化时创建空的数据源
+    this.dataSource = new DynamicFileDataSource(this.treeControl, this.fileService, []);
   }
 
   ngOnInit() {
@@ -97,7 +171,18 @@ export class FileTreeComponent implements OnInit {
   loadRootPath(path = this.rootPath): void {
     const files = this.fileService.readDir(path);
     console.log('Loaded root path files:', files);
-    this.dataChange.next(files as FileNode[]);
+    
+    // 转换为扁平节点格式
+    const flatFiles: FlatFileNode[] = files.map(file => ({
+      expandable: !file.isLeaf,
+      title: file.title,
+      level: 0,
+      key: file.key,
+      isLeaf: file.isLeaf,
+      path: file['path']
+    }));
+    
+    this.dataSource.setRootData(flatFiles);
   }
 
   // 判断节点是否有子节点
@@ -113,8 +198,8 @@ export class FileTreeComponent implements OnInit {
   }
 
   // 获取当前数据
-  getCurrentData(): FileNode[] {
-    return this.dataChange.value;
+  getCurrentData(): FlatFileNode[] {
+    return this.dataSource.getCurrentData();
   }
 
   openFolder(folder: FlatFileNode) {
@@ -123,14 +208,11 @@ export class FileTreeComponent implements OnInit {
       this.treeControl.collapse(folder);
     } else {
       this.treeControl.expand(folder);
-      // 加载子文件夹内容
-      const files = this.fileService.readDir(folder.path);
-      console.log('Loaded folder files:', files);
-
+      // 动态数据源会自动处理子文件夹的加载
     }
   }
 
-  openFile(file) {
+  openFile(file: FlatFileNode) {
     this.selectedFile = file.path;
     this.selectedFileChange.emit(file);
   }
@@ -148,7 +230,7 @@ export class FileTreeComponent implements OnInit {
 
   // 检查文件列表是否为空
   isEmpty(): boolean {
-    return this.dataChange.value.length === 0;
+    return this.dataSource.getCurrentData().length === 0;
   }
 
   refresh() {
